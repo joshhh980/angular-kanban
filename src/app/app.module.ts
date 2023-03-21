a0d55d41126accc49859e6e69a7dff54cece8fe4
Joshua <knana6523@gmail.com>
2023-03-21 16:46:34 +0000

    feat: add drag and drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './components/task/task.component';
 import { BoardComponent } from './components/board/board.component';
@@ -18,7 +18,8 @@ import { ParameterizePipe } from './pipes/parameterize.pipe';
     ParameterizePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, Input, OnInit } from '@angular/core';
 import { ITask } from 'src/app/models/task';
 
@@ -17,6 +18,19 @@ export class ListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 
 }
